0f5a25a8e87983d7f3b69e264fb0d3cfaffb794b
matos-nelson <nmatos6@gatech.edu>
2024-12-02 13:04:48 -0600

    Day 3 setup.

---
+++ 2024/day3/Main.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main (String [] args) {
+        BufferedReader reader;
+        List<String> lines = new ArrayList<String>();
+
+        try {
+            reader = new BufferedReader(new FileReader("test_input.txt"));
+            //reader = new BufferedReader(new FileReader("input.txt"));
+            String line = reader.readLine();
+
+            while (line != null) {
+                lines.add(line);
+                line = reader.readLine();
+            }
+
+            reader.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+
+        String[] arr = new String[lines.size()];
+        arr = lines.toArray(arr);
+        System.out.println(problem_p1(arr));
+        System.out.println(problem_p2(arr));
+    }
+
+    public static int problem_p1(String [] inputs) {
+        System.out.println("Part 1....");
+        return 0;
+    }
+
+    public static int problem_p2(String [] inputs) {
+        System.out.println("Part 2....");
+        return 0;
+    }
+}
