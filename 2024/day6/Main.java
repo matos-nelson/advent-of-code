0a5ab35ffa15eef4d5d7a137704b03451cd9421c
matos-nelson <nmatos6@gatech.edu>
2024-12-06 17:13:52 -0600

    Day 6 complete

--- 2024/day6/Main.java
+++ 2024/day6/Main.java
@@ -9,8 +9,8 @@ public class Main {
         List<String> lines = new ArrayList<String>();
 
         try {
-            reader = new BufferedReader(new FileReader("test_input.txt"));
-            //reader = new BufferedReader(new FileReader("input.txt"));
+            //reader = new BufferedReader(new FileReader("test_input.txt"));
+            reader = new BufferedReader(new FileReader("input.txt"));
             String line = reader.readLine();
 
             while (line != null) {
@@ -29,13 +29,162 @@ public class Main {
         System.out.println(problem_p2(arr));
     }
 
-    public static int problem_p1(String [] inputs) {
-        System.out.println("Part 1....");
-        return 0;
+    static int [][] moves = new int[][] {
+        { -1, 0 },
+        {  0, 1 },
+        {  1, 0 },
+        {  0, -1}
+    };
+
+    static boolean isInBound(int n, int m, int row, int col) {
+        if(row < 0 || row >= n || col < 0 || col >= m) {
+            return false;
+        }
+
+        return true;
     }
 
-    public static int problem_p2(String [] inputs) {
+    public static long problem_p1(String [] inputs) {
+        System.out.println("Part 1....");
+
+        int n = inputs.length;
+        int m = inputs[0].length();
+        char [][] grid = new char[n][m];
+
+        int row = -1;
+        int col = -1;
+        for(int i = 0; i < n; i++) {
+
+            for(int j = 0; j < m; j++) {
+
+                grid[i][j] = inputs[i].charAt(j);
+                if(grid[i][j] == '^') {
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+
+        int index = 0;
+        while(isInBound(n, m, row, col)) {
+
+            grid[row][col] = 'X';
+
+            int r = row + moves[index][0];
+            int c = col + moves[index][1];
+
+            if(isInBound(n, m, r, c) && grid[r][c] == '#') {
+                if(++index >= moves.length) {
+                    index = 0;
+                }
+            } else {
+                row = r;
+                col = c;
+            }
+        }
+
+        long result = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 'X') {
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static boolean isCycle(char [][] grid, int row, int col, int [][] visited, int n, int m) {
+
+        int index = 0;
+        while(isInBound(n, m, row, col)) {
+
+            visited[row][col]++;
+            if(visited[row][col] > moves.length) {
+                return true;
+            }
+
+            int r = row + moves[index][0];
+            int c = col + moves[index][1];
+
+            if(isInBound(n, m, r, c) && grid[r][c] == '#') {
+                if(++index >= moves.length) {
+                    index = 0;
+                }
+            } else {
+                row = r;
+                col = c;
+            }
+        }
+
+        return false;
+    }
+
+    public static long problem_p2(String [] inputs) {
         System.out.println("Part 2....");
-        return 0;
+
+        int n = inputs.length;
+        int m = inputs[0].length();
+        char [][] grid = new char[n][m];
+        int [][] count = new int[n][m];
+
+        int row = -1;
+        int col = -1;
+        for(int i = 0; i < n; i++) {
+
+            for(int j = 0; j < m; j++) {
+
+                grid[i][j] = inputs[i].charAt(j);
+                if(grid[i][j] == '^') {
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+
+        int index = 0;
+        int startRow = row;
+        int startCol = col;
+        List<int []> cells = new ArrayList<>();
+        while(isInBound(n, m, row, col)) {
+
+            if(count[row][col] == 0) {
+                count[row][col] = 1;
+                cells.add(new int[]{row, col});
+            }
+
+            int r = row + moves[index][0];
+            int c = col + moves[index][1];
+
+            if(isInBound(n, m, r, c) && grid[r][c] == '#') {
+                if(++index >= moves.length) {
+                    index = 0;
+                }
+            } else {
+                row = r;
+                col = c;
+            }
+        }
+
+        long result = 0;
+        for(int [] cell : cells) {
+
+            count = new int[n][m];
+            int r = cell[0];
+            int c = cell[1];
+
+            grid[r][c] = '#';
+
+            boolean isCycle = isCycle(grid, startRow, startCol, count, n, m);
+            if(isCycle) {
+                result++;
+            }
+
+            grid[r][c] = '.';
+
+        }
+
+        return result;
     }
 }
