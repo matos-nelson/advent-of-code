402c781939bbfe50248cee278a187bb4f068df62
matos-nelson <nmatos6@gatech.edu>
2024-12-09 15:44:07 -0600

    Day 10 template

---
+++ 2024/day10/Main.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.*;
+
+public class Main {
+    public static void main (String [] args) {
+        BufferedReader reader;
+        List<String> lines = new ArrayList<String>();
+
+        try {
+            reader = new BufferedReader(new FileReader("test_input.txt"));
+            //reader = new BufferedReader(new FileReader("input.txt"));
+            String line = reader.readLine();
+
+            while (line != null) {
+                lines.add(line);
+                line = reader.readLine();
+            }
+
+            reader.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+
+        String[] arr = new String[lines.size()];
+        arr = lines.toArray(arr);
+
+        long startTime = System.nanoTime();
+        System.out.println(problem_p1(arr));
+        long endTime = System.nanoTime();
+
+        System.out.println("P1 Time (s): " + TimeUnit.NANOSECONDS.toSeconds(endTime - startTime) + "\n");
+
+        startTime = System.nanoTime();
+        System.out.println(problem_p2(arr));
+        endTime = System.nanoTime();
+
+        System.out.println("P2 Time (s): " + TimeUnit.NANOSECONDS.toSeconds(endTime - startTime));
+    }
+
+    public static long problem_p1(String [] inputs) {
+        System.out.println("Part 1....");
+        return 0L;
+    }
+
+    public static long problem_p2(String [] inputs) {
+        System.out.println("Part 2....");
+        return 0L;
+    }
+}
