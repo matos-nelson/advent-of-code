929aa2e7cb7ff8bb3831331dad245cd35afb91b2
Nelson Matos <nmatos@Nelsons-MacBook-Pro.local>
2025-12-11 15:24:32 -0600

    Day 11 part 2

--- 2025/day11/Main.java
+++ 2025/day11/Main.java
@@ -41,7 +41,7 @@ public class Main {
         System.out.println("P2 Time (s): " + TimeUnit.NANOSECONDS.toSeconds(endTime - startTime));
     }
 
-    static long dfs(String node, String end, Map<String, Set<String>> graph, Set<String> visited) {
+    static long dfs(String node, String end, Map<String, Set<String>> graph, Set<String> visited)  {
 
         if(visited.contains(node)) {
             return 0L;
@@ -64,9 +64,75 @@ public class Main {
         return count;
     }
 
+    static long dfs(String node, String end, int state, Map<String, Set<String>> graph, long [][][] memo, Map<String, Integer> nodeIds)  {
+
+        int id = nodeIds.get(node);
+        if(memo[id][state][0] == 1) {
+            return memo[id][state][1];
+        }
+
+        long result = 0;
+        if(node.equals(end)) {
+            result = state == 3 ? 1 : 0;
+        } else {
+
+            boolean isDacSeen = state == 1 || state == 3;
+            boolean isFftSeen = state == 2 || state == 3;
+
+            if(node.equals("dac")) {
+                isDacSeen = true;
+            } else if(node.equals("fft")) {
+                isFftSeen = true;
+            }
+            
+            int newState = 0;
+            if(!isDacSeen && !isFftSeen) {
+                newState = 0;
+            } else if(isDacSeen && !isFftSeen) {
+                newState = 1;
+            } else if(!isDacSeen && isFftSeen) {
+                newState = 2;
+            } else {
+                newState = 3;
+            }
+
+            for(String v : graph.get(node)) {
+                result += dfs(v, end, newState, graph, memo, nodeIds);
+            }
+        }
+
+        memo[id][state][0] = 1;
+        memo[id][state][1] = result;
+
+        return result;
+    }
+
     public static long problem_p1(String [] inputs) {
         System.out.println("Part 1....");
 
+        Map<String, Set<String>> graph = new HashMap<>();
+        for(String input : inputs) {
+
+            String [] nodeToEdges = input.split(":");
+            String node = nodeToEdges[0].trim();
+
+            String [] edges = nodeToEdges[1].trim().split(" ");
+            Set<String> set = new HashSet<>(List.of(edges));
+            graph.put(node, set);
+        }
+
+        String start = "you";
+        String end = "out";
+        if(!graph.containsKey(start)) {
+            return 0L;
+        }
+
+        long result = dfs(start, end, graph, new HashSet<>());
+        return result;
+    }
+
+    public static long problem_p2(String [] inputs) {
+        System.out.println("Part 2....");
         Map<String, Set<String>> graph = new HashMap<>();
         for(String input : inputs) {
 
@@ -79,12 +145,30 @@ public class Main {
             graph.put(node, set);
         }
 
-        long result = dfs("you", "out", graph, new HashSet<>());
-        return result;
-    }
+        String start = "svr";
+        String end = "out";
+        if(!graph.containsKey(start)) {
+            return 0L;
+        }
 
-    public static long problem_p2(String [] inputs) {
-        System.out.println("Part 2....");
-        return 0L;
+        int id = 0;
+        Map<String, Integer> nodeIds = new HashMap<>();
+        for(String key : graph.keySet()) {
+            if(!nodeIds.containsKey(key)) {
+                nodeIds.put(key, id);
+                id++;
+            }
+
+            for(String v : graph.get(key)) {
+                if(!nodeIds.containsKey(v)) {
+                    nodeIds.put(v, id);
+                    id++;
+                }
+            }
+        }
+
+        long [][][] memo = new long[id][4][2];
+        long result = dfs(start, end, 0, graph, memo, nodeIds);
+        return result;
     }
 }
